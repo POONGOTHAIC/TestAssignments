59f4b6fdc122e2ed7996a06ac0d30ca386ba0716
Poongothai C <poongothai.sathishn@gmail.com>
2022-05-03 00:45:54 +0530

    TestLeaf Assignment

---
+++ src/main/java/week2/day1/LearningString.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+public class LearningString {
+	public static void main(String[] args) {
+String text = "testleaf";
+char charAt = text.charAt(1);
+char charAt1 = text.charAt(5);
+System.out.println(charAt);
+System.out.println(charAt1);
+		
+	}
+	}
+
+
+
